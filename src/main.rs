3c1bdbd25f60e08f3b774db886d424d42fd421ca
Krouton <me+git@tokinia.me>
2021-10-30 15:43:15 +0900

    Feels good.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{self, stdin, Read};
+
+fn main() -> io::Result<()> {
+    let mut buffer = String::new();
+    stdin().read_to_string(&mut buffer)?;
+
+    let x: Vec<_> = buffer
+        .lines()
+        .filter(|line| line.split('\t').count() > 3)
+        .map(|line| line.split('\t').skip(2).collect::<Vec<_>>().join("\t"))
+        .collect();
+    println!("{}", x.join("\n"));
+    Ok(())
 }
